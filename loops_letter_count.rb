28ae1a5110bd3c1d4e191d78f7771f9cb67b0fdb
William Price <price.james.william@gmail.com>
2023-03-24 05:57:50 +0000

    entered solutions

--- loops_letter_count.rb
+++ loops_letter_count.rb
@@ -1,9 +1,9 @@
 # Write a program that:
-# 
+#
 # Asks the user to input a word and counts from 1 to however long the word is.
-# 
+#
 # Example (`apple` is the input):
-# 
+#
 # "Enter a word:"
 # apple
 # 1
@@ -14,3 +14,14 @@
 # "apple is 5 letters long!"
 
 p "Enter a word:"
+user_word = gets.chomp.downcase
+
+word_count = user_word.length
+
+count = 1
+while count <= word_count
+  puts count
+  count += 1
+end
+
+puts "#{user_word} is #{word_count} letters long"
